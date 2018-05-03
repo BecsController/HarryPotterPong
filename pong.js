090e1c6178b19a91b0e985f92451981558fd6c5f
Rebecca Dugdale <bec.dugdale16@gmail.com>
2018-05-03 14:59:06 +1200

    target pieces

---
+++ pong.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', start)
+
+function start(){
+
+var playerOnePiece = document.getElementsByClassName("gryfGamePiece")[0];
+var playerTwoPiece = document.getElementsByClassName("slyGamePiece")[0];
+
+}
